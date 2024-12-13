f82274db4915c8add5f1ee72bf2be28f5832795f
Arasailnp <Splain3@lsu.edu>
2024-12-13 07:44:00 -0600

    circus

--- sketch.js
+++ sketch.js
@@ -5,31 +5,32 @@ function preload(){
   img1 =loadImage("open box.png")
   img2 =loadImage("box2.png")
   img3 =loadImage("box.jpg")
-  
+  bg = loadImage("circus.jpg")
+  music = loadSound("music.mp3")
 }
 function setup() {
   createCanvas(800, 800);
   box245 = loadImage("box2.png")
   box123 = loadImage("box.jpg")
-  x= 86
+  x= 228
   y= 359
+  music.play()
   
-  
-  collectItem = loadImage("tophat.jpg")
+  hat = loadImage("tophat.jpg")
  
 }
 
 function draw() {
-  background(220);
+  image(bg,0,0,800,800)
   console.log(finish)
-   ellipse(130,420,50,50)
   
-  text('Help The Man Find His Hat', 160, 70)
-  textSize(8)
-  text('press a for a hint', 160, 100)
+  text('Help The Man Find His Hat', 300, 70)
+  textSize(10)
+  text('press the mouse for a hint', 500, 10)
   textSize(22)
+  text('press "a" to lift the box', 300,100)
   image(box123, x, y, 80, 120)
-  function keyIsPressed () {
+  function keyIsPressed () {}
   if (key === 'a'){
     value = 225
     image(box123, x, y- 150, 80, 120) 
@@ -37,7 +38,7 @@ function draw() {
     noStroke()
     rect(x,y,80,120)
     text('Its not Here!',123, 106)
-  }
+   
   }
    //image(img1);
   function draw() {
@@ -48,29 +49,13 @@ function draw() {
     
   }
  // image(img2);
-  image(box123, 321, 358, 80, 120)
+  image(box123, 493, 358, 80, 120)
   if (mouseIsPressed){
     noStroke()
-    fill(220)
-    rect(321, 358, 80, 120)
-    
-    image(box123, 321, 358- 150, 80, 120) }
-  function ball(){
- // image(box123, 600, 358, 80, 120)
-    fill(10)
-    noStroke()
-    ellipse(630,420,50,50)
-    if (x > 100 && x <170 && y > 100) {
-    safe = false;
-   setup();  //start again
-  }
-    
-  }
-  //function ( ){
+    fill('#fdf9f0')
+    rect(493, 358, 80, 120)
+  image(hat,  228, 380, 80, 80 )  
+    image(box123, 493, 358- 150, 80, 120) }
  
+  
     
-  //}
-  //mouse show
-   fill(255, 60, 100);
-  text("(" + mouseX + ", " + mouseY + ")", mouseX, mouseY);
-}
